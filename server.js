324387902e00956e427f9f249011db600a1f43bf
Stephen-Roach <stephenroach21@gmail.com>
2020-10-28 21:23:38 -0500

    added ejs package ejs template and view folder

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 const fetch = require('node-fetch');
-const pgp = require("pg-promise")();
+const pgp = require('pg-promise')();
 const Sequelize = require('sequelize');
+const ejs = require('ejs');
 
-
-const { DB_LOCAL, DB_PASSWORD, DB_NAME, DB_USER, PORT } = require("./envConfig");
+const {
+  DB_LOCAL,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_USER,
+  PORT,
+} = require('./envConfig');
 
 const app = express();
 
@@ -17,14 +23,20 @@ const test = require('./router/apiTest');
 
 app.use(bodyParser.json());
 
-app.use("/", express.static(__dirname + "/public"));
-app.use("/css",express.static(__dirname + "/css"));
-app.use("/js", express.static(__dirname + "/js"));
+app.use('/', express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/js'));
+
+// Below is setting the view to look for an ejs file
+app.set('view engine', 'ejs');
+
+// Rendering the homepage through ejs, can be moved to a router file if you all want
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.use('/heartbeat', heartbeat);
 app.use('/test', test);
 
-
-  app.listen(PORT, ()=> {
-    console.log(`The server at port ${PORT} is listening...`);
-})
+app.listen(PORT, () => {
+  console.log(`The server at port ${PORT} is listening...`);
+});
