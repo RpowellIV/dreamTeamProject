610d85da2e1e56b03b2cb9f67c99b63aed3fdc49
JohnLoftin <jgloftin@gmail.com>
2020-11-03 20:55:21 -0500

    adding for merge

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 $().ready(() => {
-    // const sequelize = require('../config/sequelize')
-    import * as sequelize from "./config/sequelize.js";
-    
-
     $('#search-btn').click((e) => {
         e.preventDefault();
         const searchVal = $('#search-bar').val();
