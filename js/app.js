0777743d329e9a5b4cfe93a4eda2172cf21ecea9
JohnLoftin <jgloftin@gmail.com>
2020-11-03 19:04:38 -0500

    fixing conflict

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 $().ready(() => {
-    // const routes = require('./router/main')
     // const sequelize = require('../config/sequelize')
-    // import * as sequelize from "./config/sequelize.js";
+    import * as sequelize from "./config/sequelize.js";
     
 
     $('#search-btn').click((e) => {
         e.preventDefault();
         const searchVal = $('#search-bar').val();
-        console.log(searchVal);
-        let results = sequelize.query("select * from User where firstName = " + searchVal)
-        console.log(results)
+        let results = sequelize.query("select * from User where firstName = " + searchVal);
+        console.log(results);
     })
 })
