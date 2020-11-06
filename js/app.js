7960c419448e7e4ba9ad9ab6be0fe76925424676
JohnLoftin <jgloftin@gmail.com>
2020-11-06 15:11:49 -0500

    removed console log

--- js/app.js
+++ js/app.js
@@ -13,11 +13,10 @@ $().ready(() => {
         searchVal.toLowerCase();
         jobTitles.map((job) => {
             if(job.title.toLowerCase().includes(searchVal)) {
-                console.log(job.title);
                 $("#jobs-container").append(`
                     <div class="job">
                         <div class="card" style="width: 100%;">
-                            <h3>${job.title} - ${job.location}</h3>
+                            <h3>${job.title} - ${job.city}, ${job.state}</h3>
                             <p><sub>${job.companyName}</sub></p>
                             <p>${job.description}</p>
                         </div>
