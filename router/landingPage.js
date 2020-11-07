df39d865e6046043b74acc2bcb4c616389ec2a12
Robert <r.powelliv@gmail.com>
2020-11-07 13:21:33 -0500

    BIO

---
+++ router/landingPage.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pgp = require("pg-promise")();
+const router = express();
+const db = require('../models');
+
+
+// router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false }));
+
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/')
+}
+
+// Homepage
+router.get("/",ensureAuthenticated, (req, res) => {
+    res.render("pages/landingPage");
+})
+
+router.post('/', async (req, res) => {
+
+    console.log("Here")
+    console.log(req.user.id);
+    
+    const {isEmployer, bio} = req.body;
+
+    console.log(req.body)
+    console.log(isEmployer,bio)
+
+    if(isEmployer==="false") {
+        const user = await db.Users.update(
+
+            {
+                isEmployer: false,
+                bio: bio
+            },
+    
+            { 
+                where: {
+                        id: req.user.id 
+                    }, 
+            }
+        )
+
+        res.render('pages/employee')
+
+    } else {
+        const user = await db.Users.update(
+
+            {
+                isEmployer: true,
+                bio: bio
+            },
+    
+            { 
+                where: {
+                        id: req.user.id 
+                    }, 
+            }
+        )
+
+        res.render('pages/employer')
+    }
+});
+
+
+    module.exports = router;
