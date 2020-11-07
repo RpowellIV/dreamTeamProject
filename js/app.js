7b416c278d03596c544147513a83f9801afa4f63
JohnLoftin <jgloftin@gmail.com>
2020-11-07 12:51:28 -0500

    changed modal layout

--- js/app.js
+++ js/app.js
@@ -18,24 +18,22 @@ $().ready(() => {
                         <div class="card" style="width: 100%;">
                             <h3>${job.title} - ${job.city}, ${job.state}</h3>
                             <p><sub>${job.companyName}</sub></p>
-                            <p>${job.description}</p>
+                            <p id="descText">${job.description}</p>
                             <button type="button" class="btn btn-info btn-lg" id="jobModalBtn">Read More</button>
                         </div>
                         <!-- Modal -->
                         <div class="modal fade" id="jobModal" role="dialog">
-                            <div class="modal-dialog">
-    
+                            <div class="modal-dialog"> 
                                 <!-- Modal content-->
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal" style="float: left; margin-left: 0px;">&times;</button>
-                                            <h4 class="modal-title">${job.title}</h4>
-                                            <p>${job.location}</p>
-                                            <p><sub>${job.companyName}</sub></p>
+                                        <h4 class="modal-title">${job.title}</h4>
                                     </div>
                                     <div class="modal-body">
+                                        <p><sub>${job.companyName}</sub></p>
                                         <p>${job.description}</p>
                                         <p><sub>${job.email}</sub></p>
+                                        <p>${job.location}</p>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -53,5 +51,4 @@ $().ready(() => {
             }
         })
     }
-    
 })
