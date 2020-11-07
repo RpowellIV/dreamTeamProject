5bb481f12659397e7471633164012546962ccbee
Robert <r.powelliv@gmail.com>
2020-11-07 13:48:40 -0500

    user updates

--- router/landingPage.js
+++ router/landingPage.js
@@ -26,17 +26,17 @@ router.post('/', async (req, res) => {
     console.log("Here")
     console.log(req.user.id);
     
-    const {isEmployer, bio} = req.body;
+    const {isEmployer, bio, companyName } = req.body;
 
     console.log(req.body)
-    console.log(isEmployer,bio)
+    console.log(isEmployer,bio,companyName)
 
     if(isEmployer==="false") {
         const user = await db.Users.update(
 
             {
                 isEmployer: false,
-                bio: bio
+                bio: bio,
             },
     
             { 
@@ -53,7 +53,8 @@ router.post('/', async (req, res) => {
 
             {
                 isEmployer: true,
-                bio: bio
+                bio: bio,
+                companyName: companyName
             },
     
             { 
@@ -65,9 +66,6 @@ router.post('/', async (req, res) => {
 
         res.render('pages/employer')
     }
-    // } else {
-    //     return console.error(err);
-    // }
 });
 
 
