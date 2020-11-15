61201e0b102a49f81b5d4b37b85bd144657192d9
bensvarnum <bens.varnum@gmail.com>
2020-11-15 18:53:59 -0500

    added form to employer to create jobs

--- router/jobs.js
+++ router/jobs.js
@@ -16,20 +16,54 @@ router.get("/", async (req, res) => {
         attributes: ['id', 'title', 'companyName', 'description', 'email', 'city', 'state', 'location']
     })
     res.json({
-        is: "working", 
+        is: "working",
         jobs
     })
 
+})
+
+router.post('/employerjob', async (req, res) => {
+    try {
+        const {
+
+            jCompany,
+            jAbout,
+            jUrl,
+            jLocation,
+            jState,
+            jCity,
+            jTitle } = req.body;
+
+        await db.create(
+
+            {
+                title: jTitle,
+                companyName: jCompany,
+                description: jAbout,
+                email: jUrl,
+                location: jLocation,
+                city: jCity,
+                state: jState,
+                numApplied: null,
+                whoApplied: null
+            }, {}
+        )
+        res.render("pages/employer")
+        // res.redirect('/employer')
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
 })
 
 router.post('/', async (req, res) => {
 
     console.log("Here")
     console.log(req.user.id);
-    
+
     const {
-        addJob, 
-        outJob, 
+        addJob,
+        outJob,
         jCompany,
         jAbout,
         jUrl,
@@ -54,7 +88,7 @@ router.post('/', async (req, res) => {
             state: jState,
             numApplied: null,
             whoApplied: null
-        },{}
+        }, {}
     )
 
     const userJobs = await db2.create(
@@ -63,10 +97,10 @@ router.post('/', async (req, res) => {
             hasBoth: "yes",
             JobId: parseInt(addJob) || Jobs.id,
             UserId: req.user.id
-        },{}
-        )
-    
-  
+        }, {}
+    )
+
+
 });
 
-module.exports = {router};
+module.exports = { router };
